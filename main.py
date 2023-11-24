0f1a677a5db226796b03306d4e663f50b08f7cc7
ProstoEvgenii <98259817+ProstoEvgenii@users.noreply.github.com>
2023-11-24 17:19:32 +0300

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import date
 
-TOKEN = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
+TOKEN = ""
 
 
 class VKApi:
